490741777bb6a628a7066d640268bc1924056e21
NinaLuangphet <110043381+NinaLuangphet@users.noreply.github.com>
2022-07-26 22:25:38 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,49 @@
+
+
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = './index.html'
+}
+
+// eslint-disable-next-line no-unused-vars
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://www.mecallapi.com/api/login");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({
+    "username": username,
+    "password": password,
+  }));
+  xhttp.onreadystatechange = function () {
+    // eslint-disable-next-line eqeqeq
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      // eslint-disable-next-line eqeqeq
+      if (objects['status'] == 'ok') {
+        localStorage.setItem("jwt", objects['accessToken']);
+        // eslint-disable-next-line no-undef
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = './index.html';
+          }
+        });
+      } else {
+        // eslint-disable-next-line no-undef
+        Swal.fire({
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    }
+  };
+  return false;
+}
